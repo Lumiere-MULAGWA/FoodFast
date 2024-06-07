447ee8216f77b96796d6251517f1d7a0266c1b21
21mb358@esisalama.org <21mb358@esisalama.org>
2024-06-07 09:18:09 +0200

    commit

--- app/src/main/java/com/example/foodfast/MainActivity.kt
+++ app/src/main/java/com/example/foodfast/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+
             FoodFastTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
